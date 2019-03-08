dd0a2c77916b34b3abbd2eb52c90bccae7750e28
Axel COUDRAY <axelcoudray1@gmail.com>
2019-03-08 09:39:34 +0100

    [EVO]

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:page_view_indicator/page_view_indicator.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MaterialApp(
       title: 'Hupomnesis',
       home: MyHomePage(),
     ),
@@ -22,8 +22,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   SharedPreferences data;
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  TextEditingController nameController = TextEditingController();
-  TextEditingController textController = TextEditingController();
+  final ValueNotifier<int> pageIndexNotifier = ValueNotifier<int>(0);
+  bool _editing = false;
   List<String> names;
   List<String> texts;
 
@@ -31,33 +31,27 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     getInstance();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    nameController.dispose();
-    textController.dispose();
+    flutterLocalNotificationsPlugin.initialize(const InitializationSettings(AndroidInitializationSettings('app_icon'), null));
+    _repeatNotification();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Column(
+      body: names == null 
+      ? const CircularProgressIndicator() 
+      : Stack(
         children: <Widget>[
-          Container(
-            height: 450, 
-            child: reminders(),
-          ),
+          reminders(),
+          _buildIndex(),
         ],
       ),
     );
   }
 
   Widget reminders() {
-    return ListView.builder(
-      padding: const EdgeInsets.only(left: 38),
+    return PageView.builder(
+      onPageChanged: (int index) => pageIndexNotifier.value = index,
       itemBuilder: (BuildContext context, int index) => _itemBuilder(context, index),
       itemCount: 4,
       scrollDirection: Axis.horizontal,
@@ -65,95 +59,100 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _itemBuilder(BuildContext context, int index) {
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _editing = true;
+            });
+          },
+          child: _editing 
+            ? textField(index, 1, 13, 'reminder\'s name', ValueToChange.name)
+            : Text(names[index], style: const TextStyle(fontSize: 28, color: Colors.grey, fontFamily: 'Roboto-Bold'),),
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _editing = true;
+            });
+          },
+          child: _editing 
+            ? textField(index, 9, 380, 'what should you remind ?', ValueToChange.text)
+            : Text(names[index], style: const TextStyle(fontSize: 28, color: Colors.grey, fontFamily: 'Roboto-Bold'),),
+        )
+      ],
+    );
+  }
 
+  Widget _buildIndex() {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        width: 300.0,
-        child: Column(
-          children: <Widget> [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: Text(names[index], style: const TextStyle(fontSize: 28, color: Colors.grey, fontFamily: 'Roboto-Bold'),),
-                ),
-                IconButton(icon: const Icon(Icons.create), color: Colors.blue, onPressed: () => _showDialog(index)),
-              ],
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.9),
+      child: PageViewIndicator(
+      pageIndexNotifier: pageIndexNotifier,
+      length: 4,
+      normalBuilder: (AnimationController animationController) => Circle(
+            size: 8.0,
+            color: Colors.black87,
+          ),
+      highlightedBuilder: (AnimationController animationController) => ScaleTransition(
+            scale: CurvedAnimation(
+              parent: animationController,
+              curve: Curves.ease,
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(12, 38, 12, 0),
-              child: Text.rich(
-                TextSpan(text: texts[index], style: const TextStyle(fontSize: 22, color: Colors.black, fontFamily: 'Roboto-Light'),),
-                textAlign: TextAlign.justify,
-              ),
+            child: Circle(
+              size: 12.0,
+              color: Colors.black,
             ),
-          ],
-        ),
-      ),
+          ),
+    ),
     );
   }
 
-  void _showDialog(int index) {
-    showDialog<SimpleDialog>(
-      context: context,
-      builder: (BuildContext context) {
-        return SimpleDialog(
-          title: const Text('What do you want to learn today ?', style: TextStyle(fontFamily: 'Roboto-Light'),),
-          contentPadding: const EdgeInsets.all(12),
-          backgroundColor: Colors.white,
-          children: <Widget>[
-            textField(nameController, index, 1, 13, 'reminder\'s name', ValueToChange.name),
-            textField(textController, index, 9, 380, 'what should you remind ?', ValueToChange.text),
-          ],
-        );
-      },
-    );
-  }
-
-  Widget textField(TextEditingController controller, int index, int maxLines, int maxLength, String text, ValueToChange vtc) {
+  Widget textField(int index, int maxLines, int maxLength, String text, ValueToChange vtc) {
     return Column(
       children: <Widget>[
         TextField(
-          controller: controller,
           decoration: InputDecoration(
             hintText: text,
           ),
           maxLines: maxLines,
           maxLength: maxLength,
+          onSubmitted: (String value) => updateData(index, vtc, value),
         ),
-        RaisedButton(
-          color: Colors.blue,
-          child: const Text('Submit!', style: TextStyle(fontFamily: 'Roboto-Light'),),
-          onPressed: () => updateData(index, vtc)
-        )
       ],
     );
   }
 
   Future<void> getInstance() async {
     data = await SharedPreferences.getInstance();
-    names = data.getStringList('names') ?? <String>['', '', '', ''];
-    texts = data.getStringList('texts') ?? <String>['', '', '', ''];
+    setState(() {
+      names = data.getStringList('names') ?? <String>['a', 'c', 'f', 'g'];
+      texts = data.getStringList('texts') ?? <String>['b', 'd', 'e', 'h'];
+    });
   }
 
-  void updateData(int index, ValueToChange vtc) {
+  void updateData(int index, ValueToChange vtc, String value) {
     setState(() {
+      _editing = false;
       getInstance();
       if(vtc == ValueToChange.text) {
-        texts[index] = textController.text;
+        texts[index] = value;
         data.setStringList('texts', texts);
-        Navigator.of(context).pop();
       } else {
-        names[index] = nameController.text.toUpperCase();
+        names[index] = value.toUpperCase();
         data.setStringList('names', names);
       }
     });
+  }
+
+  Future<void> _repeatNotification() async {
+    final AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'repeating channel id',
+      'repeating channel name',
+      'repeating description');
+    final NotificationDetails platformChannelSpecifics = NotificationDetails(androidPlatformChannelSpecifics, null);
+    await flutterLocalNotificationsPlugin.periodicallyShow(0, 'repeating title','repeating body', RepeatInterval.EveryMinute, platformChannelSpecifics);
   }
 }
 
