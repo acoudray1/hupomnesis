b06bc671b47efbf037c4d7ffe82e7709632391a2
Axel COUDRAY <axelcoudray1@gmail.com>
2019-03-10 12:23:39 +0100

    [EVO]

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:page_view_indicator/page_view_indicator.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -23,7 +25,7 @@ class _MyHomePageState extends State<MyHomePage> {
   SharedPreferences data;
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
   final ValueNotifier<int> pageIndexNotifier = ValueNotifier<int>(0);
-  bool _editing = false;
+  bool introduction;
   List<String> names;
   List<String> texts;
 
@@ -32,14 +34,15 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     getInstance();
     flutterLocalNotificationsPlugin.initialize(const InitializationSettings(AndroidInitializationSettings('app_icon'), null));
-    _repeatNotification();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      resizeToAvoidBottomPadding: false,
       body: names == null 
-      ? const CircularProgressIndicator() 
+      ? const CircularProgressIndicator()
       : Stack(
         children: <Widget>[
           reminders(),
@@ -50,6 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget reminders() {
+    _repeatNotification();
     return PageView.builder(
       onPageChanged: (int index) => pageIndexNotifier.value = index,
       itemBuilder: (BuildContext context, int index) => _itemBuilder(context, index),
@@ -61,80 +65,67 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _itemBuilder(BuildContext context, int index) {
     return Column(
       children: <Widget>[
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              _editing = true;
-            });
-          },
-          child: _editing 
-            ? textField(index, 1, 13, 'reminder\'s name', ValueToChange.name)
-            : Text(names[index], style: const TextStyle(fontSize: 28, color: Colors.grey, fontFamily: 'Roboto-Bold'),),
-        ),
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              _editing = true;
-            });
-          },
-          child: _editing 
-            ? textField(index, 9, 380, 'what should you remind ?', ValueToChange.text)
-            : Text(names[index], style: const TextStyle(fontSize: 28, color: Colors.grey, fontFamily: 'Roboto-Bold'),),
-        )
+        textField(index, 1, names[index], ValueToChange.name, TextAlign.start, 'Title'),
+        textField(index, 15, texts[index], ValueToChange.text, TextAlign.justify, 'Note'),
       ],
     );
   }
 
   Widget _buildIndex() {
     return Padding(
-      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.9),
+      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.95),
       child: PageViewIndicator(
-      pageIndexNotifier: pageIndexNotifier,
-      length: 4,
-      normalBuilder: (AnimationController animationController) => Circle(
-            size: 8.0,
-            color: Colors.black87,
+        pageIndexNotifier: pageIndexNotifier,
+        length: 4,
+        normalBuilder: (AnimationController animationController) => Circle(
+          size: 8.0,
+          color: Colors.black54,
+        ),
+        highlightedBuilder: (AnimationController animationController) => ScaleTransition(
+          scale: CurvedAnimation(
+            parent: animationController,
+            curve: Curves.ease,
           ),
-      highlightedBuilder: (AnimationController animationController) => ScaleTransition(
-            scale: CurvedAnimation(
-              parent: animationController,
-              curve: Curves.ease,
-            ),
-            child: Circle(
-              size: 12.0,
-              color: Colors.black,
-            ),
+          child: Circle(
+            size: 11.0,
+            color: Colors.black54,
           ),
-    ),
+        ),
+      ),
     );
   }
 
-  Widget textField(int index, int maxLines, int maxLength, String text, ValueToChange vtc) {
-    return Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(
-            hintText: text,
-          ),
-          maxLines: maxLines,
-          maxLength: maxLength,
-          onSubmitted: (String value) => updateData(index, vtc, value),
+  Widget textField(int index, int maxLines, String text, ValueToChange vtc, TextAlign textAlign, String hint) {
+    final TextEditingController controller = TextEditingController();
+    controller.text = text;
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(5, 24, 5, 12),
+      child: TextField(
+        decoration: InputDecoration.collapsed(
+          hintText: hint,
+          hintStyle: const TextStyle(fontSize: 28, color: Colors.black54, fontFamily: 'Roboto-Light'),
         ),
-      ],
+        controller: controller,
+        textAlign: textAlign,
+        style: const TextStyle(fontSize: 28, color: Colors.black, fontFamily: 'Roboto-Light'),
+        maxLines: maxLines,
+        textInputAction: TextInputAction.done,
+        onSubmitted: (String value) => updateData(index, vtc, value),
+      ),
     );
   }
 
   Future<void> getInstance() async {
     data = await SharedPreferences.getInstance();
     setState(() {
-      names = data.getStringList('names') ?? <String>['a', 'c', 'f', 'g'];
-      texts = data.getStringList('texts') ?? <String>['b', 'd', 'e', 'h'];
+      names = data.getStringList('names') ?? <String>['', '', '', ''];
+      texts = data.getStringList('texts') ?? <String>['', '', '', ''];
+      introduction = data.getBool('introduction') ?? false;
     });
   }
 
   void updateData(int index, ValueToChange vtc, String value) {
     setState(() {
-      _editing = false;
       getInstance();
       if(vtc == ValueToChange.text) {
         texts[index] = value;
@@ -147,12 +138,13 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> _repeatNotification() async {
+    final int rnd = Random().nextInt(4);
     final AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
       'repeating channel id',
       'repeating channel name',
       'repeating description');
     final NotificationDetails platformChannelSpecifics = NotificationDetails(androidPlatformChannelSpecifics, null);
-    await flutterLocalNotificationsPlugin.periodicallyShow(0, 'repeating title','repeating body', RepeatInterval.EveryMinute, platformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.periodicallyShow(0, '${names[rnd]}','${texts[rnd]}', RepeatInterval.EveryMinute, platformChannelSpecifics);
   }
 }
 
