5a6364c5cbcbbf9dfe1c9d4bc95612a9520a5f36
Axel COUDRAY <axelcoudray1@gmail.com>
2019-03-10 16:07:11 +0100

    [END] : Only need to create the flare

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,27 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:page_view_indicator/page_view_indicator.dart';
 import 'package:flare_flutter/flare_actor.dart';
 
-void main() {
-  runApp(MaterialApp(
-      title: 'Hupomnesis',
-      home: MyHomePage(),
-    ),
-  );
+void main() {runApp(MaterialApp(title: 'Hupomnesis', home: Home()));
   SystemChrome.setEnabledSystemUIOverlays(<SystemUiOverlay>[]);
 }
 
-class MyHomePage extends StatefulWidget {
+class Home extends StatefulWidget {
  @override
- _MyHomePageState createState() => _MyHomePageState();
+ _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeState extends State<Home> {
   SharedPreferences data;
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  final ValueNotifier<int> pageIndexNotifier = ValueNotifier<int>(0);
+  FlutterLocalNotificationsPlugin localNotif = FlutterLocalNotificationsPlugin();
+  final ValueNotifier<int> pIndex = ValueNotifier<int>(0);
   bool introduction;
-  List<String> names;
-  List<String> texts;
+  List<String> names, texts;
 
   @override
   void initState() {
     super.initState();
     getInstance();
-    flutterLocalNotificationsPlugin.initialize(const InitializationSettings(AndroidInitializationSettings('app_icon'), null));
+    localNotif.initialize(const InitializationSettings(AndroidInitializationSettings('app_icon'), null));
   }
 
   @override
@@ -41,33 +35,25 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       resizeToAvoidBottomPadding: false,
-      body: names == null 
+      body: names == null && introduction == null
       ? const CircularProgressIndicator()
+      : introduction == false && names != null
+      ? checkIntro()
       : Stack(
         children: <Widget>[
-          reminders(),
+          PageView.builder(
+            onPageChanged: (int index) => pIndex.value = index,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(children: <Widget>[
+                textField(index, 1, names[index], ValueToChange.name, TextAlign.start, 'Title'),
+                textField(index, 15, texts[index], ValueToChange.text, TextAlign.justify, 'Note'),
+              ]);
+            },
+            itemCount: 4,
+          ),
           _buildIndex(),
         ],
       ),
-    );
-  }
-
-  Widget reminders() {
-    _repeatNotification();
-    return PageView.builder(
-      onPageChanged: (int index) => pageIndexNotifier.value = index,
-      itemBuilder: (BuildContext context, int index) => _itemBuilder(context, index),
-      itemCount: 4,
-      scrollDirection: Axis.horizontal,
-    );
-  }
-
-  Widget _itemBuilder(BuildContext context, int index) {
-    return Column(
-      children: <Widget>[
-        textField(index, 1, names[index], ValueToChange.name, TextAlign.start, 'Title'),
-        textField(index, 15, texts[index], ValueToChange.text, TextAlign.justify, 'Note'),
-      ],
     );
   }
 
@@ -75,21 +61,12 @@ class _MyHomePageState extends State<MyHomePage> {
     return Padding(
       padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.95),
       child: PageViewIndicator(
-        pageIndexNotifier: pageIndexNotifier,
+        pageIndexNotifier: pIndex,
         length: 4,
-        normalBuilder: (AnimationController animationController) => Circle(
-          size: 8.0,
-          color: Colors.black54,
-        ),
-        highlightedBuilder: (AnimationController animationController) => ScaleTransition(
-          scale: CurvedAnimation(
-            parent: animationController,
-            curve: Curves.ease,
-          ),
-          child: Circle(
-            size: 11.0,
-            color: Colors.black54,
-          ),
+        normalBuilder: (AnimationController aController) => Circle(size: 8.0, color: Colors.black54),
+        highlightedBuilder: (AnimationController aController) => ScaleTransition(
+          scale: CurvedAnimation(parent: aController, curve: Curves.ease),
+          child: Circle(size: 11.0, color: Colors.black54),
         ),
       ),
     );
@@ -101,10 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Padding(
       padding: const EdgeInsets.fromLTRB(5, 24, 5, 12),
       child: TextField(
-        decoration: InputDecoration.collapsed(
-          hintText: hint,
-          hintStyle: const TextStyle(fontSize: 28, color: Colors.black54, fontFamily: 'Roboto-Light'),
-        ),
+        decoration: InputDecoration.collapsed(hintText: hint, hintStyle: const TextStyle(fontSize: 28, color: Colors.black54, fontFamily: 'Roboto-Light')),
         controller: controller,
         textAlign: textAlign,
         style: const TextStyle(fontSize: 28, color: Colors.black, fontFamily: 'Roboto-Light'),
@@ -124,7 +98,31 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Widget checkIntro() {
+    return Stack(
+      children: <Widget> [
+        Center(
+          child: FlareActor('assets/animations/intro.flr',
+            alignment: Alignment.center,
+            fit: BoxFit.fitWidth,
+            animation: 'frame',
+          ),
+        ),
+        IconButton(
+          padding: EdgeInsets.only(left: MediaQuery.of(context).size.width*0.48, top: MediaQuery.of(context).size.height*0.9),
+          icon: const Icon(Icons.touch_app),
+          color: Colors.blue,
+          onPressed: () => setState(() {
+            data.setBool('introduction', true);
+            getInstance();
+          }),
+        ),
+      ],
+    );
+  }
+
   void updateData(int index, ValueToChange vtc, String value) {
+    _repeatNotification();
     setState(() {
       getInstance();
       if(vtc == ValueToChange.text) {
@@ -139,12 +137,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _repeatNotification() async {
     final int rnd = Random().nextInt(4);
-    final AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
-      'repeating channel id',
-      'repeating channel name',
-      'repeating description');
-    final NotificationDetails platformChannelSpecifics = NotificationDetails(androidPlatformChannelSpecifics, null);
-    await flutterLocalNotificationsPlugin.periodicallyShow(0, '${names[rnd]}','${texts[rnd]}', RepeatInterval.EveryMinute, platformChannelSpecifics);
+    final AndroidNotificationDetails androidChannel = AndroidNotificationDetails('repeating channel id', 'repeating channel name', 'repeating description');
+    final NotificationDetails channel = NotificationDetails(androidChannel, null);
+    await localNotif.periodicallyShow(0, '${names[rnd]}','${texts[rnd]}', RepeatInterval.Hourly, channel);
   }
 }
 
