c24e39aaf6815ed3e97f78bf36c670718850e933
Axel COUDRAY <axelcoudray1@gmail.com>
2019-04-22 16:08:43 +0200

    HUP-3 : WIP - allowing to write file on device

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:hupomnesis/src/model/note.dart';
 import 'package:hupomnesis/src/resources/note_manager.dart';
 
@@ -12,4 +14,25 @@ class Repository {
 
   // Method used to fetch notes from a json file
   Future<List<Note>> fetchAllNotes() => noteManager.fetchNotes('assets/notes.json');
+
+  // Method that allows us to get the Document path where documents are stored
+  Future<String> get _localPath async {
+    final dynamic directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  // Create the reference to the file we want to create
+  Future<File> _localFile(String name) async {
+    final String path = await _localPath;
+
+    return File('$path/$name');
+  }
+
+  Future<File> writeCounter(int counter) async {
+  final file = await _localFile;
+
+  // Write the file
+  return file.writeAsString('$counter');
+}
 }
