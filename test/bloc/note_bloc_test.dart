5a7cdbef1894ac5fd78d2c501a12936708379484
Axel COUDRAY <axelcoudray1@gmail.com>
2019-04-27 17:24:42 +0200

    HUP4 : update test
    not working yet

--- test/bloc/note_bloc_test.dart
+++ test/bloc/note_bloc_test.dart
@@ -1,21 +1,23 @@
+import 'package:hupomnesis/config.dart';
 import 'package:hupomnesis/src/bloc/note_bloc.dart';
 import 'package:hupomnesis/src/model/note.dart';
+import 'package:hupomnesis/src/resources/note_manager.dart';
 import 'package:hupomnesis/src/resources/repository.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('NoteBloc.createNote() creates a note, add it to Note.class and write it in json', () {
+  test('NoteBloc.createNote() creates a note, add it to Note.class and write it in json', () async {
     final Repository repository = Repository();
     final NoteBloc noteBloc = NoteBloc();
+    final NoteManager noteManager = NoteManager();
     const String name = 'NOTE_TEST';
     const String text = 'This note is created for a test';
+    String writtenData;
 
     noteBloc.createNote(name, text);
-    repository.
-
+    writtenData = await repository.readData(properties['NOTES_FILE_NAME']);
 
     expect(noteBloc.notes, contains(Note(name: name, text: text)));
-
-
+    expect(noteBloc.notes, equals(noteManager.noteFromJson(writtenData)));
   });
 }
