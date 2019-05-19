aa29b68793927a4b81983183425dc63b3d48627d
Axel COUDRAY <axelcoudray1@gmail.com>
2019-05-19 17:02:32 +0200

    HUP-9 : Notes in md on cards

--- lib/src/views/note_page/build_card.dart
+++ lib/src/views/note_page/build_card.dart
@@ -1,27 +1,37 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:hupomnesis/src/bloc/note_bloc/note_bloc.dart';
 import 'package:hupomnesis/src/bloc/note_bloc/note_selection_bloc.dart';
 import 'package:hupomnesis/src/model/enum_color_selected.dart';
 import 'package:hupomnesis/src/model/enum_status.dart';
 import 'package:hupomnesis/src/model/note.dart';
 import 'package:hupomnesis/src/views/note_edition_page/note_edition_page.dart';
-import 'package:hupomnesis/theme/style_texte.dart';
 
 ///
 /// Widget that builds a card
 /// 
 Widget buildCard(BuildContext context, int index, List<Note> notes, NoteSelectionBloc noteSelectionBloc, Status status, NoteBloc noteBloc) {
   Color _borderColor = Colors.grey;
+  double _borderWidth = 1.0;
   Color _noteColor;
+  double _elevation = 0.0;
 
   return StreamBuilder<bool>(
     stream: noteSelectionBloc.isSelectingStream,
     initialData: notes[index].isSelected,
     builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
       if (snapshot.hasData) {
-        notes[index].isSelected ? _borderColor = Colors.blue : _borderColor = Colors.grey;
+        if (notes[index].isSelected) {
+          _borderColor = Colors.blue;
+          _borderWidth = 2.5;
+          _elevation = 3.0;
+        } else if (!notes[index].isSelected) {
+          _borderColor = Colors.grey;
+          _borderWidth = 1.0;
+          _elevation = 0.0;
+        }
 
         switch (notes[index].colorSelected) {
           case ColorSelected.BLUE:
@@ -57,9 +67,9 @@ Widget buildCard(BuildContext context, int index, List<Note> notes, NoteSelectio
                 left: 2.0,
                 right: 2.0,
                 child: Card(
-                  elevation: notes[index].isSelected ? 3 : 0,
+                  elevation: _elevation,
                   shape: RoundedRectangleBorder(
-                    side: BorderSide(color: _borderColor, width: 1.0),
+                    side: BorderSide(color: _borderColor, width: _borderWidth),
                     borderRadius: BorderRadius.circular(6.0)
                   ),
                   color: Colors.white,
@@ -80,41 +90,22 @@ Widget buildCard(BuildContext context, int index, List<Note> notes, NoteSelectio
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Row(
+                          notes[index].colorSelected != ColorSelected.NORMAL 
+                          ? Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
-                              Text('${notes[index].name}', style: Style.subtitleTextStyle, textAlign: TextAlign.start,),
+                              const SizedBox(width: 13.0),
                               Icon(Icons.brightness_1, color: _noteColor,),
                             ],
-                          ),
-                          const SizedBox(height: 1.0,),
-                          Text('${notes[index].text}', style: Style.commonTextStyle, textAlign: TextAlign.justify,)
+                          ) : Container(),
+                          MarkdownBody(data: notes[index].text,),
+                          //Text('${}', style: Style.commonTextStyle, textAlign: TextAlign.justify,),
                         ],
                       ),
                     ),
                   ),
                 ),
               ),
-              notes[index].isSelected ? Positioned(
-                top: 10.0,
-                bottom: 10.0,
-                left: 10.0,
-                right: 10.0,
-                child: GestureDetector(
-                  onTap: () => noteSelectionBloc.handleNoteToggle(notes[index]),
-                  child: ClipRect(
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
-                      child: Container(
-                        //padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.5),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ) : Container(),
             ],
           ),
         );
